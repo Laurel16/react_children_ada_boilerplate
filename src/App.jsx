c686dafe05255b8a09f151de8b76970d1fe512c3
Laurel16 <laure.leter@gmail.com>
2023-12-06 16:23:45 +0100

    just a button

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Button from './components/Button'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <section>
+      <Button text="Joyeux Nöel" />
+    </section>
+    </>
+  )
+}
+
+export default App
